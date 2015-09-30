9ad8a9de5ac9f39541750ad03970a3f064845711
lichenbuliren <617106268@qq.com>
2015-09-30 14:20:14 +0800

    支持lib目录下面所有的.ttf字体文件压缩

--- index.js
+++ index.js
@@ -17,12 +17,14 @@ exports.register = function(commander) {
     var argvPath = process.argv[3],
         projectPath = fis.project.getProjectPath(),
         fontSrc = path.resolve(projectPath, '../../lib/KaiGenGothicCN-Light.ttf'),
+        fontSrcArr = [],
         // 文案文件路径，可以是目录，也可以是单个文件
         strTargetPath,
         // 字体文件生成目标路径
         fontDestPath,
         // 文件内容字符串
         strContent;
+    fontSrcArr = getFontSrcArr(path.resolve(projectPath, '../../lib'), /\.ttf$/);
     if (argvPath == '-h') {
         var content = [
             '',
@@ -34,7 +36,6 @@ exports.register = function(commander) {
             '   makefont [pathname]         Read the [pathname] text resource',
             ''
         ].join('\n');
-        console.log(content);
         return;
     }
     // 根据是否传递参数来决定生成的字体文件地址
@@ -50,27 +51,59 @@ exports.register = function(commander) {
         strTargetPath = path.resolve(projectPath, 'test');
         fontDestPath = path.resolve(projectPath, 'static/global/fonts');
     }
-    if (!fs.existsSync(fontSrc)) {
-        console.error('字体源文件路径错误，请在项目根目录运行!!');
+    if (!fs.existsSync(fontSrcArr[0])) {
+        console.error('字体源文件路径错误，请在项目根目录运行，例如 /source/cn/ ');
         return false;
     }
 
     strContent = readFileContent(strTargetPath).join('');
-    // minfy
-    var fontmin = new Fontmin()
-        .src(fontSrc)
-        .use(Fontmin.glyph({
-            text: strContent
-        }))
-        .use(Fontmin.ttf2eot())
-        .use(Fontmin.ttf2woff())
-        .use(Fontmin.ttf2svg())
-        .dest(fontDestPath);
-    fontmin.run(function(err, files) {
-        if (err) {
-            throw err;
-        }
+    console.log('总共 %d 个原始字体文件,开始压缩中。。。。。。', fontSrcArr.length);
+    for (var i = 0; i < fontSrcArr.length; i++) {
+        var count = 0;
+        var fontmin = new Fontmin()
+            .src(fontSrcArr[i])
+            .use(Fontmin.glyph({
+                text: strContent
+            }))
+            .use(Fontmin.ttf2eot())
+            .use(Fontmin.ttf2woff())
+            .use(Fontmin.ttf2svg())
+            .dest(fontDestPath);
+        fontmin.run(function(err, files) {
+            if (err) {
+                throw err;
+            } else {
+                console.log('恭喜，%d 个字体文件生成成功！', files.length);
+            }
+        })
+    }
+}
+
+/**
+ * 批量压缩字体
+ * @param  {[type]} fontSrcArr [description]
+ * @param  {[type]} strContent [description]
+ * @return {[type]}            [description]
+ */
+// function fontMinify(fontSrcArr, strContent) {
+//     var fontminArr = [];
+
+// }
+
+/**
+ * 获取指定后缀名字体文件路径数组
+ * @param string dir 字体文件目录
+ * @param  {[type]} reg [description]
+ * @return {[type]}     [description]
+ */
+function getFontSrcArr(dir, reg) {
+    var tmpArr = [];
+    travel(dir, function(fileList) {
+        tmpArr = fileList.filter(function(elem) {
+            return reg.test(elem);
+        });
     });
+    return tmpArr;
 }
 
 /**
